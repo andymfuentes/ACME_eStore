1bbbe31a56d23b6185b08d0e52c4c26d5a9768cf
Andy Fuentes <andymfuentes@yahoo.com>
2022-11-07 15:58:27 -0800

    Initial Commit

---
+++ ACME_eStore/ACME_eStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ACME_eStoreApp.swift
+//  ACME_eStore
+//
+//  Created by Andy Fuentes on 2022-11-07.
+//
+
+import SwiftUI
+
+@main
+struct ACME_eStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ACME_eStore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ACME_eStore
+//
+//  Created by Andy Fuentes on 2022-11-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
